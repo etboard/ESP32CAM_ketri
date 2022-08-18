7709130b8671f9b7c0e757117becc44012d48cde
손철수 <mrbr@ketri.re.kr>
2022-08-18 12:07:05 +0900

    change wifi information

--- src/arduino/CameraWebServer_ketri/CameraWebServer_ketri.ino
+++ src/arduino/CameraWebServer_ketri/CameraWebServer_ketri.ino
@@ -35,8 +35,8 @@
 // ===========================
 // Enter your WiFi credentials
 // ===========================
-const char* ssid = "fhm";
-const char* password = "fhm12345";
+const char* ssid = "ketri-guest";
+const char* password = "0617212484";
 
 void startCameraServer();
 
